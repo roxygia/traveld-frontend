db8008a26df8a502ce7b3028d65b5b88cd1b9782
roxygia <62426542+roxygia@users.noreply.github.com>
2020-09-19 10:44:51 +0800

    added nav for home and trip pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Nav from "./components/nav/Nav";
+import HomePage from "./pages/HomePage";
+import TripPage from "./pages/TripPage";
 
-function App() {
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/trip">
+            <TripPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
-
-export default App;
---
+++ src/pages/TripPage.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function TripPage() {
+    return <h1>This is the Trip Details page.</h1>
+}
+
+export default TripPage;
